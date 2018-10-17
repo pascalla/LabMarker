a935cf371d71195444de3ca23ef87b5e3aeda67a
Joshua Blackman <joshuadennisblackman@gmail.com>
2018-10-17 19:40:42 +0100

    Update User Creation Form
    - Updated form to new schema
    - Added new student "role"

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -10,16 +10,16 @@
                 <div class="card-body">
                   {{ Form::open(array('route' => 'user.store')) }}
                     <div class="form-group">
+                      {{ Form::label('identifier', 'Identifier:') }}
+                      {{ Form::text('identifier', old('identifier'), array('class' => 'form-control')) }}
+                    </div>
+                    <div class="form-group">
                       {{ Form::label('name', 'Name:') }}
                       {{ Form::text('name', old('name'), array('class' => 'form-control')) }}
                     </div>
                     <div class="form-group">
-                      {{ Form::label('email', 'Email:') }}
-                      {{ Form::text('email', old('email'), array('class' => 'form-control')) }}
-                    </div>
-                    <div class="form-group">
                       {{ Form::label('role', 'Role:') }}
-                      {{ Form::select('role', array('Marker' => 'Marker', 'Lecturer' => 'Lecturer', 'Overseer' => 'Overseer', 'Admin' => 'Admin', 'System Admin' => 'System Administrator'), 
+                      {{ Form::select('role', array('Student' => 'Student', 'Marker' => 'Marker', 'Lecturer' => 'Lecturer', 'Overseer' => 'Overseer', 'Admin' => 'Admin', 'System Admin' => 'System Administrator'),
                           old('role'), array('class' => 'form-control')) }}
                     </div>
                     <div class="form-group">
