150cd65d3f7315a6097d7c6b0d6afe63d5cbcfc5
Joshua Blackman <joshuadennisblackman@gmail.com>
2018-10-19 13:58:45 +0100

    Update Edit Labs
    - Added edit page for labs
    - Included "Year" for upcoming feature

--- resources/views/lab/edit.blade.php
+++ resources/views/lab/edit.blade.php
@@ -9,9 +9,21 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
           <div class="card">
-            <div class="card-header"></div>
+            <div class="card-header">Update {{ $lab->name }}</div>
             <div class="card-body">
-
+              {{ Form::open(array('url' => route('lab.update', $lab->id) )) }}
+                <div class="form-group">
+                  {{ Form::label('course_code', 'Course Code:')}}
+                  {{ Form::text('course_code', $lab->course_code, array('class' => 'form-control')) }}
+                </div>
+                <div class="form-group">
+                  {{ Form::label('year', 'Year:')}}
+                  {{ Form::text('year', old('year'), array('class' => 'form-control')) }}
+                </div>
+                <div class="form-group">
+                  {{ Form::submit('Update', array('class' => 'btn  btn-primary btn-block'))}}
+                </div>
+              {{ Form::close() }}
             </div>
           </div>
         </div>
