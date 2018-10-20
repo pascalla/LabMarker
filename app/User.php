10374f33e9a0a657506810e5415fdcde006affc7
Joshua Blackman <joshuadennisblackman@gmail.com>
2018-10-20 16:37:05 +0100

    Update User Model
    - Added some comments
    - Only check active enrollments for isEnrolled

--- app/User.php
+++ app/User.php
@@ -33,15 +33,18 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    // Get name for dropdown
     public function getDropDownName(){
       return $this->surname . ', ' . $this->firstname . ' (' . $this->identifier . ')';
     }
 
+    // Get Collection of labs that this user has enrolled in (past and present)
     public function enrolledLabs(){
       return $this->belongsToMany('App\Lab', 'enrollments');
     }
 
+    // Check if user is enrolled in in lab.
     public function isEnrolled($lab){
-      return $this->enrolledLabs()->get()->contains($lab);
+      return $this->enrolledLabs()->where('deleted_at', null)->get()->contains($lab);
     }
 }
