a67479cd355faafd260f0caf14075b2e4df4cd2f
Joshua Blackman <joshuadennisblackman@gmail.com>
2018-11-02 12:33:41 +0000

    Added Student Marks to Home
    - If the user is enrolled in any modules, on login all modules and task progress is shown

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
+use App\TaskProgress;
+use App\Task;
 use App\User;
 use App\Lab;
 use Auth;
@@ -31,6 +33,38 @@ class HomeController extends Controller
     public function index()
     {
         $labs = Lab::all();
-        return view('home')->with('labs', $labs);
+
+        $user = Auth::user();
+
+
+        $studentLabs = array();
+
+        foreach($user->enrolledLabs()->where('enrollments.deleted_at', null)->get() as $lab){
+          $tasks = $lab->getTasks()->get();
+          $progress = collect([]);
+
+          foreach($tasks as $task){
+            // If task has been completed (aka database entry, push it to array)
+            if($user->checkTaskProgress($task)){
+              $progress->push($user->getTaskProgress($task)->first());
+            // else create a new task progress with status 0 and push to array
+            } else {
+              $taskProgress = new TaskProgress;
+              $taskProgress->user_id = $user->id;
+              $taskProgress->lab_id = $lab->id;
+              $taskProgress->task_id = $task->id;
+              $taskProgress->status = 0;
+              $progress->push($taskProgress);
+            }
+
+          }
+
+          $studentLabs[] = array('lab' => $lab, 'tasks' => $tasks, 'progress' => $progress);
+
+        }
+
+        //dd($studentLabs);
+
+        return view('home')->with('labs', $labs)->with('studentLabs', $studentLabs);
     }
 }
