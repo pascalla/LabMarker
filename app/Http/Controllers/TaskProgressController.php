f5469c18d6fa07098bc2d9faaf53cfeb51e8fe7e
Joshua Blackman <joshuadennisblackman@gmail.com>
2018-11-05 02:10:45 +0000

    Update Task Progress Logic
    - Now takes into account the deadlines of the Task

--- app/Http/Controllers/TaskProgressController.php
+++ app/Http/Controllers/TaskProgressController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\TaskProgress;
+use App\Task;
+use Carbon;
 use Validator;
 
 class TaskProgressController extends Controller
@@ -59,13 +61,36 @@ class TaskProgressController extends Controller
         $errors = $validator->errors();
         return $errors->toJson();
       } else {
+        $task = Task::findOrFail($request->task_id);
 
+        // Check if we there are deadlines in place
+        if(isset($task->full_marks) || isset($task->half_marks)){
+          // If we are within the full marks deadline and its set give em full marks!
+          if(time() < strtotime($task->full_marks) && isset($task->full_marks)){
+            $marks = $task->marks;
+            $status = 1;
+          // If they didn't make full marks, but half marks, give em half marks!
+          } else if(time() < strtotime($task->half_marks) && isset($task->half_marks)){
+            $marks = $task->marks/2;
+            $status = 2;
+          // Otherwise they missed the deadline :( give em 0
+          } else {
+            $marks = 0;
+            $status = 3;
+          }
+        }
+
+        // If there was no deadlines, full marks for everyone!
+        $marks = $task->marks;
+        $status = 1;
 
         $taskProgress = new TaskProgress;
         $taskProgress->user_id= $request->user_id;
         $taskProgress->task_id = $request->task_id;
         $taskProgress->lab_id = $request->lab;
-        $taskProgress->status = 1;
+        $taskProgress->marks = $marks;
+        $taskProgress->status = $status;
+
         $taskProgress->save();
 
         return response(['status' => 'success']);
