525dc71c352c1b11a193d13f3d1d5f31b401b77c
Joshua Blackman <joshuadennisblackman@gmail.com>
2018-11-05 02:11:08 +0000

    Update Task Progress
    - Added easy method for getting icon for task progress

--- app/TaskProgress.php
+++ app/TaskProgress.php
@@ -20,10 +20,33 @@ class TaskProgress extends Model
       'user_id', 'task_id', 'status',
   ];
 
+  // Status Meanings
+  // 0 - Incomplete
+  // 1 - Complete Full Marks
+  // 2 -- Complete Half Marks
+  // 3 -- Complete No Marks
+
   /**
   * The table associated with the model.
   *
   * @var string
   */
   protected $table = 'tasks_progress';
+
+  // Used for easier icon displaying
+  // These relate to classes in foundation icons
+  public function getProgressIcon(){
+    switch($this->status){
+      case '0':
+        return 'fi-x red';
+      case '1':
+        return 'fi-check green';
+      case '2':
+        return 'fi-check yellow';
+      case '3':
+        return 'fi-check red';
+      default:
+        return 'fi-x red';
+    }
+  }
 }
